9b00287296dfc9a0b35941cdd68c1b8f12529ef4
Maryam Alimohammadi <gh.familimarymohamadi90@gmail.com@gmail.com>
2020-02-14 23:16:10 +0200

    Initial Commit

---
+++ Feed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Feed
+//
+//  Created by Maryam Alimohammadi on 2/14/20.
+//  Copyright © 2020 Maryam Alimohammadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
